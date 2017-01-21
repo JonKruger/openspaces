6e4de449c70140b4a08121637113ec63d33eec30
Jon Kruger <github@jonkruger.com>
2017-01-20 23:19:40 -0500

    working on saving

--- client/src/services/SessionService.js
+++ client/src/services/SessionService.js
@@ -10,6 +10,7 @@ export function saveSession(session) {
   // TODO: call the server to save stuff
   return fetch("http://localhost:3000/sessions/save", {
     method: 'post',
+    headers: new Headers({'Content-Type': 'application/json'}),
     body: session
   })
     .then(response => { return response.json(); });
